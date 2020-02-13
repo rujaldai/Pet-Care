74c3e7de6d28f4df9be58badcbe0c1dad2a18937
Rujal Manandhar <rujaldai@gmail.com>
2020-02-13 23:24:49 +0545

    [#28] implement update product and upload image fix

--- end_points/product/ProductEndPoints.js
+++ end_points/product/ProductEndPoints.js
@@ -3,9 +3,10 @@ module.exports = (function() {
     var productController = require("../../controller/ProductController.js");
     var authController = require("../../controller/AuthController");
 
-routes.post('/add', authController.verifyToken, productController.validator, productController.updateIntoProduct);
+routes.post('/add', authController.verifyToken, productController.validator, productController.insertIntoProduct);
 routes.get('/:userId/all', productController.fetchAllByUserId);
 routes.get('/all', productController.fetchAllProducts);
+routes.put('/update', authController.verifyToken, productController.validator, productController.updateIntoProduct);
 
 // routes.delete('/:id', authController.verifyToken, userController.deleteUser);
 
--- end_points/upload/UploadEndpoint.js
+++ end_points/upload/UploadEndpoint.js
@@ -1,17 +1,14 @@
 module.exports = (function() {
-var routes = require("express").Router();
-var upload = require("../../controller/UploadController");
-var path = require('path');
-routes.post("/", upload.single('imageFile'), (req, res) => {
-    console.log(res.file);    
-    res.status(200);
-    console.log(__dirname);
-    console.log(req.file);
-    req.file.path = _baseDir + "/"+ req.file.path;
-    console.log(req.file.path);
-    res.json(req.file);
-        
-    });
-
-return routes;
-})();
+    var routes = require("express").Router();
+    var upload = require("../../controller/UploadController");
+    var path = require('path');
+    routes.post("/", upload.single('imageFile'), (req, res) => {
+        console.log(res.file);    
+        res.status(200);
+        console.log(__dirname);
+        console.log(req.file);
+        res.json(req.file);
+        });
+    
+    return routes;
+    })();
